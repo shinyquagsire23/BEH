87326f03bffa9e7673f193cb2479c0a63b3f7f0a
Alexander Nicholi <alex@pokecommunity.com>
2015-07-12 18:46:49 -0400

    Fixed typos in comment header

--- source/GBAUtils/rom.d
+++ source/GBAUtils/rom.d
@@ -7,20 +7,20 @@
  * `rom.d`                                                                    *
  * "classes for managing Pokémon GBA ROMs"                                    *
  *                                                                            *
- *                         This file is part of Lapis.                        *
+ *                         This file is part of BEH.                          *
  *                                                                            *
- * Lapis is free software: you can redistribute it and/or modify it under the *
+ *  BEH is free software: you can redistribute it and/or modify it under the  *
  *  terms of the GNU General Public License as published by the Free Software *
  * Foundation, either version 3 of the License, or (at your option) any later *
  *                                  version.                                  *
  *                                                                            *
- * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY  *
+ *   BEH is distributed in the hope that it will be useful, but WITHOUT ANY   *
  *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS *
  *   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more   *
  *                                  details.                                  *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License along   *
- *          with Lapis.  If not, see <http://www.gnu.org/licenses/>.          *
+ *           with BEH.  If not, see <http://www.gnu.org/licenses/>.           *
  *                                                                            *
  *****************************************************************************/
 
